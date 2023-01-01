88ec16ae7e53797e2cf2634920a9411833adde31
Makitweb <makitweb@gmail.com>
2023-01-01 20:23:17 +0530

    Load project

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title></title>
+	<title>Create Livewire pagination with Search filter and Sorting in Laravel</title>
 
 	<!-- Fontawesome -->
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
